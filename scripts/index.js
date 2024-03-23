a4666f050cfb6169942ec24d41de9da9782595a8
rasool1992 <somanxx@yahoo.com>
2024-03-24 02:26:06 +0300

    add delete old methods

--- scripts/index.js
+++ scripts/index.js
@@ -3,24 +3,33 @@ let bookmarkUrlInput = document.querySelector('.bookmark-url');
 let tableContent = document.querySelector('.table-content');
 let btnSubmit = document.querySelector('.btn-submit');
 let bookmarkList = JSON.parse(localStorage.getItem('bookmarkList')) || [];
+displayContent();
+
+
+// let deleteItem = document.querySelector('.delete');
+// deleteItem.addEventListener('click',function(e){
+//   if(e.target.classList.contains('delete') === true){
+//     e.target.parentNode.closest('tr').remove();
+//   }
+// })
 
 let bookmarkNameReg = /^[a-zA-Z]{3,}$/;
 let bookmarkUrkReg = /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
 
+
 btnSubmit.addEventListener('click',function(){
   if(valideInput(bookmarkNameReg,bookmarkNameInput)& valideInput(bookmarkUrkReg,bookmarkUrlInput))
   {
-   
     let bookmarkObj = {
       name: bookmarkNameInput.value,
       url : bookmarkUrlInput.value
     }
     bookmarkList.push(bookmarkObj);
     localStorage.setItem('bookmarkList',JSON.stringify(bookmarkList));
+    resetFields();
     displayContent();
   }else{
-
-    console.log('erro');
+    alert('Invalid Input');
   }
 });
 
@@ -30,7 +39,6 @@ bookmarkNameInput.addEventListener('input',function(){
 
 bookmarkUrlInput.addEventListener('input', function(){
   valideInput(bookmarkUrkReg,bookmarkUrlInput);
-
 })
 
 function displayContent(){
@@ -39,21 +47,36 @@ function displayContent(){
     htmlContent += `<tr>
     <td>${i+1}</td> 
       <td>${bookmarkList[i].name}</td>
-      <td><a href="${bookmarkList[i].url}" class="text-dark">Visit</a></td>
-      <td><a href="#" class="text-dark">Delete</a></td>
+      <td><a href="${bookmarkList[i].url}" target="_blank" class="text-dark">Visit</a></td>
+      <td><a onclick='removeItem(${i})' href="#" class="text-dark delete">Delete</a></td>
     </tr>`
   }
   tableContent.innerHTML = htmlContent;
 }
-displayContent();
+
 
 function valideInput(patern , input){
   let res = patern.test(input.value);
   if (res === true ) {
     input.classList.add('is-valid');
     input.classList.remove('is-invalid');
+    return true;
   }else{
     input.classList.add('is-invalid');
     input.classList.remove('is-valid');
+    return false;
   }
+}
+function resetFields(){
+  bookmarkNameInput.value = null;
+  bookmarkNameInput.classList.remove('is-valid','is-invalid');
+  bookmarkUrlInput.value = null;
+  bookmarkUrlInput.classList.remove('is-valid','is-invalid');
+}
+
+// AlterMethod for Delete
+function removeItem(index){
+  bookmarkList.splice(index,1);
+  localStorage.setItem('bookmarkList',JSON.stringify(bookmarkList));
+  displayContent();
 }
